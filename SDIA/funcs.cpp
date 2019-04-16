c72458ed3fd3912c83b211dfbdef1f7e1affbea4
DevilFromSea <48997786+DevilFromSea@users.noreply.github.com>
2019-04-16 21:02:32 +0700

    Add files via upload

--- SDIA/funcs.cpp
+++ SDIA/funcs.cpp
@@ -2,36 +2,36 @@
 #include "tree.h"
 using namespace std;
 
-void add(tree **t, int x)  
+void add(tree **t, int x)
 {
-	if (!*t)             
+	if (!*t)
 	{
-		*t = new tree;              
-		(*t)->data = x;                  
+		*t = new tree;
+		(*t)->data = x;
 		(*t)->left = NULL;
-		(*t)->right = NULL;  
+		(*t)->right = NULL;
 		return;
 	}
-	if (x < (*t)->data)   
+	if (x < (*t)->data)
 	{
-		if ((*t)->left) 
-			add(&(*t)->left, x); 
-		else          
+		if ((*t)->left)
+			add(&(*t)->left, x);
+		else
 		{
-			(*t)->left = new tree;              
-			(*t)->left->left = NULL; 
-			(*t)->left->right = NULL; 
-			(*t)->left->data = x;                   
+			(*t)->left = new tree;
+			(*t)->left->left = NULL;
+			(*t)->left->right = NULL;
+			(*t)->left->data = x;
 		}
 	}
-	if (x > (*t)->data)             
+	if (x > (*t)->data)
 	{
-		if ((*t)->right != NULL) 
+		if ((*t)->right != NULL)
 			add(&(*t)->right, x);
-		else 
+		else
 		{
-			(*t)->right = new tree; 
-			(*t)->right->left = NULL; 
+			(*t)->right = new tree;
+			(*t)->right->left = NULL;
 			(*t)->right->right = NULL;
 			(*t)->right->data = x;
 		}
@@ -53,12 +53,34 @@ void buildtree(tree **t)
 
 void directed(tree *t)
 {
+	stack <tree *> S;
+	while (t) 
+	{
+		printf_s("%d\n", t->data);
 
+		if (t->left && t->right)
+		{
+			S.push(t->right);
+			t = t->left;
+		}
+		else
+			if (!t->left && !t->right && !S.empty())
+			{
+				t = S.top();
+				S.pop();
+			}
+			else
+				if (t->left)
+					t = t->left;
+				else t = t->right;
+	}
 }
 
 void postorder1(tree *t)
 {
-
+	postorder2(t->left);
+	printf_s("%d\n", t->data);
+	postorder2(t->right);
 }
 
 void postorder2(tree *t)
@@ -93,8 +115,8 @@ void postorder2(tree *t)
 void instructions()
 {
 	printf_s("How would you like to print the tree?\n"
-			 "1)Directed order\n"
-			 "2)Postorder (left child -> root -> right child)\n"
-			 "3)Postorder (left child -> right child -> root)\n"
-			 "4)Exit\n");
+		"1)Directed order\n"
+		"2)Postorder (left child -> root -> right child)\n"
+		"3)Postorder (left child -> right child -> root)\n"
+		"4)Exit\n");
 }
